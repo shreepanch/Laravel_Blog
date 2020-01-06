08192639a7cd692d6a815addf808b18b16652151
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2020-01-06 15:21:18 +0545

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
 });
+
+Route::get('/blog/show', function () {
+    return view('blog.show');
+});
+
